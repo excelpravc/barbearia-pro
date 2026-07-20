ac62222383db74f61ceb6b4822a8c7fc27894172
excelpravc <excelpravc@gmail.com>
2026-07-20 15:30:13 -0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,20 +1,46 @@
 // Importe as funções do Firebase
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
-import { getFirestore, collection, doc, getDoc, getDocs, setDoc, updateDoc, deleteDoc, query, where, onSnapshot, runTransaction } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+import { 
+  getFirestore, 
+  collection, 
+  doc, 
+  getDoc, 
+  getDocs, 
+  setDoc, 
+  updateDoc, 
+  deleteDoc, 
+  query, 
+  where, 
+  onSnapshot, 
+  runTransaction 
+} from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
-// SUAS CONFIGURAÇÕES DO FIREBASE (cole aqui as chaves do seu projeto)
+// SUAS CONFIGURAÇÕES DO FIREBASE (Atualizadas)
 const firebaseConfig = {
-  apiKey: "SUA_API_KEY",
-  authDomain: "SEU_PROJETO.firebaseapp.com",
-  projectId: "SEU_PROJETO_ID",
-  storageBucket: "SEU_PROJETO.appspot.com",
-  messagingSenderId: "SEU_MESSAGING_ID",
-  appId: "SEU_APP_ID"
+  apiKey: "AIzaSyANeVNdAYJRizus-T4AKSGuPTDQNGjq874",
+  authDomain: "barbearia-pro-d757f.firebaseapp.com",
+  projectId: "barbearia-pro-d757f",
+  storageBucket: "barbearia-pro-d757f.firebasestorage.app",
+  messagingSenderId: "737943809720",
+  appId: "1:737943809720:web:8b544038910932c1b68e4d"
 };
 
-// Inicializa Firebase
+// Inicializa o Firebase
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
-// Exporta para usar nos outros arquivos
-export { db, collection, doc, getDoc, getDocs, setDoc, updateDoc, deleteDoc, query, where, onSnapshot, runTransaction };
+// Exporta as variáveis e funções para serem usadas nos outros arquivos (client.js, AdminScripts.js, functions.js)
+export { 
+  db, 
+  collection, 
+  doc, 
+  getDoc, 
+  getDocs, 
+  setDoc, 
+  updateDoc, 
+  deleteDoc, 
+  query, 
+  where, 
+  onSnapshot, 
+  runTransaction 
+};
